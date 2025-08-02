5cb0f46f52999bf70d09a51de4899feb34e1b2a9
orkeos <orkeos98@gmail.com>
2025-08-02 12:34:12 -0600

    version 0.0.10

---
+++ Pagina-escolar-main/Pagina-escolar-master/js/menu-maestros.js
@@ -0,0 +1,24 @@
+
+function cambioaser() {
+    alert("Redirigiendo a ser");
+    // window.location.href = "materias.html";
+}
+
+function cambioasaberhacer() {
+    alert("Redirigiendo a saber hacer");
+    // window.location.href = "asistencia.html";
+}
+
+function cambioasaber() {
+    alert("Redirigiendo a saber");
+    // window.location.href = "promedio.html";
+}
+
+function cambioasistencia() {
+    alert("Redirigiendo a asistencia");
+    // window.location.href = "promedio.html";
+}
+function cambioapromedio() {
+    alert("Redirigiendo a Promedio");
+    // window.location.href = "promedio.html";
+}
